ca91cc5d636264646f2683caaa932049ff3cb526
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-10-29 03:26:22 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Footer, Navbar, Transactions, Welcome, Services } from './Components'
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { HiMenuAlt4 } from 'react-icons/hi'
+import { AiOutlineClose, AiOutlineFullscreen } from 'react-icons/ai'
+import logo from '/images/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4"></nav>
+  )
+}
+
+export default Navbar
